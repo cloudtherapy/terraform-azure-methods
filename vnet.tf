7c149ec465f278879178d37ef8d53f1219755fef
carlosmoreira-cet <cmoreira@cetechllc.com>
2021-08-10 21:05:05 -0400

    changed vnet from 10.130 to 10.157

--- vnet.tf
+++ vnet.tf
@@ -7,17 +7,17 @@ resource "azurerm_virtual_network" "methods-vnet-shared" {
   name                = "vnet-methods-shared"
   location            = azurerm_resource_group.rg-methods-shared.location
   resource_group_name = azurerm_resource_group.rg-methods-shared.name
-  address_space       = ["10.130.0.0/20"]
+  address_space       = ["10.157.0.0/20"]
   #dns_servers         = ["10.227.90.10", "10.227.90.20"]
 
   subnet {
     name           = "snet-methods-shared-private"
-    address_prefix = "10.130.0.0/24"
+    address_prefix = "10.157.0.0/24"
   }
 
   subnet {
     name           = "snet-methods-shared-public"
-    address_prefix = "10.130.1.0/24"
+    address_prefix = "10.157.1.0/24"
   }
 
   tags = {
@@ -29,7 +29,7 @@ resource "azurerm_subnet" "methods-gateway-subnet" {
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_resource_group.rg-methods-shared.name
   virtual_network_name = azurerm_virtual_network.methods-vnet-shared.name
-  address_prefixes     = ["10.130.2.0/26"]
+  address_prefixes     = ["10.157.2.0/26"]
 }
 
 resource "azurerm_local_network_gateway" "methods-tierpoint" {
@@ -74,25 +74,25 @@ resource "azurerm_virtual_network_gateway" "methods-vnet-gateway" {
   }
 }
 
-resource "azurerm_virtual_network_gateway_connection" "connection_tierpoint" {
+resource "azurerm_virtual_network_gateway_connection" "connection-tierpoint" {
   name                = "cn-shared-methods-tierpoint"
   location            = azurerm_resource_group.rg-methods-shared.location
   resource_group_name = azurerm_resource_group.rg-methods-shared.name
 
   type                       = "IPsec"
-  virtual_network_gateway_id = azurerm_virtual_network_gateway.vnet_shared_gateway.id
-  local_network_gateway_id   = azurerm_local_network_gateway.cetech_ne.id
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.methods-vnet-gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.methods-tierpoint.id
 
   shared_key = var.vpn_passphrase
 }
 
-resource "azurerm_virtual_network_gateway_connection" "connection_flemington" {
+resource "azurerm_virtual_network_gateway_connection" "connection-flemington" {
   name                = "cn-shared-methods-flemington"
   location            = azurerm_resource_group.rg-methods-shared.location
   resource_group_name = azurerm_resource_group.rg-methods-shared.name
 
   type                       = "IPsec"
-  virtual_network_gateway_id = azurerm_virtual_network_gateway.vnet_shared_gateway.id
-  local_network_gateway_id   = azurerm_local_network_gateway.cetech_nj.id
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.methods-vnet-gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.methods-flemington.id
   shared_key                 = var.vpn_passphrase
 }        
